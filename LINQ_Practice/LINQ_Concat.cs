6bfc864bcf080c4db66aae9b3ddfa946b5320966
Shivaram008 <ram.cheruku@cognine.com>
2021-06-10 15:41:06 +0530

    LINQ

---
+++ LINQ_Practice/LINQ_Concat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Practice
+{
+    class LINQ_Concat
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> b = new List<int>() { 6, 7, 8, 9, 10 };
+            var res = a.Concat(b);
+            foreach(var i in res)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+            var c = new List<string>() { "Siva", "Ram", "Prasad" };
+            var d = new List<string>() { "Bhavana", "Divya" };
+            //var res1 = a.Concat(c);
+
+            foreach(var i in c.Concat(d))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ LINQ_Practice/LINQ_Ordering.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Practice
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class LINQ_Ordering
+    {
+        public static void Display(dynamic l)
+        {
+            Console.WriteLine("{0,-5} {1,-10} {2,-5}","ID","NAME","AGE");
+            foreach(var i in l)
+            {
+                Console.WriteLine($"{i.Id,-5} {i.Name,-10} {i.Age,-5}");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            List<Student> l1 = new List<Student>() { 
+                    new Student(){Id=101,Name="Ram",Age=25},
+                    new Student(){Id=132,Name="Prasad",Age=23},
+                    new Student(){Id=101,Name="Divya",Age=20},
+                    new Student(){Id=165,Name="Bhavana",Age=18},
+                    new Student(){Id=123,Name="Siva",Age=21}
+            };
+            Console.WriteLine("Using SQL Query Syntax:");
+            var l2 = from l in l1 orderby l.Id select l;
+            Display(l2);
+            Console.WriteLine("Using Method Syntax :");
+            l2 = l1.OrderBy(x => x.Age);
+            Display(l2);
+
+            Console.WriteLine("Orderby Descending Using SQL Query Syntax:");
+            l2= from l in l1 orderby l.Id descending select l;
+            Display(l2);
+
+            Console.WriteLine("Orderby Descending Using Method Syntax:");
+            l2 = l1.OrderByDescending(x => x.Age);
+            Display(l2);
+
+            Console.WriteLine("Orderby Thenby Using SQL Query Syntax:");
+            l2 = from l in l1 orderby l.Id, l.Name select l;
+            Display(l2);
+
+            Console.WriteLine("Orderby Thenby Using Method Syntax:");
+            l2 = l1.OrderBy(x => x.Id).ThenBy(y=>y.Name);
+            Display(l2);
+
+            Console.WriteLine("Orderby ThenbyDescending Using SQL Query Syntax:");
+            l2 = from l in l1 orderby l.Id, l.Name descending select l;
+            Display(l2);
+
+            Console.WriteLine("Orderby ThenbyDescending Using Method Syntax:");
+            l2 = l1.OrderBy(x => x.Id).ThenByDescending(y => y.Name);
+            Display(l2);
+        }
+    }
+}
