766ae8e64060ce6825b91ae62a23cfe1c5c8c94d
Shivaram008 <ram.cheruku@cognine.com>
2021-06-14 15:58:51 +0530

    GroupBy

---
+++ LINQ_Practice/LINQ_GroupBy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Practice
+{
+    class LINQ_GroupBy
+    {
+        static void Main(string[] args)
+        {
+            List<Student> l1 = new List<Student>() {
+                    new Student(){Id=101,Name="Ram",Age=18},
+                    new Student(){Id=132,Name="Prasad",Age=20},
+                    new Student(){Id=101,Name="Divya",Age=20},
+                    new Student(){Id=165,Name="Bhavana",Age=18},
+                    new Student(){Id=123,Name="Siva",Age=21}
+            };
+
+            var groupBy = from s in l1 group s by s.Age;            //Query Syntax
+
+            var groupBy1 = l1.GroupBy(s => s.Age);                  //Method Syntax
+
+            foreach(var i in groupBy)
+            {
+                Console.WriteLine("AgeGroup :"+i.Key);
+                foreach(var j in i)
+                {
+                    Console.WriteLine(j.Name);
+                }
+            }
+
+            Console.WriteLine();
+
+            foreach (var i in groupBy1)
+            {
+                Console.WriteLine("AgeGroup :" + i.Key);
+                foreach (var j in i)
+                {
+                    Console.WriteLine(j.Name);
+                }
+            }
+        }
+    }
+}
